e1659a25925e22c1afb4ccb9b532e13be1f104a0
Riley Potts <120989205+Rpotts1604@users.noreply.github.com>
2023-08-20 19:34:09 +0100

    Add files

---
+++ script.js
@@ -0,0 +1,81 @@
+var about = document.getElementsByClassName("about-us-button");
+var services = document.getElementsByClassName("services-button");
+var contact = document.getElementsByClassName("contact-button");
+var feedback = document.getElementsByClassName("feedback-button")
+var i;
+
+for (i = 0; i < about.length; i++) {
+  about[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+for (i = 0; i < services.length; i++) {
+  services[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+for (i = 0; i < contact.length; i++) {
+  contact[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+for (i = 0; i < feedback.length; i++) {
+  feedback[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+var servicePanel = document.getElementsByClassName("service-panel-button");
+
+for (i = 0; i < servicePanel.length; i++) {
+  servicePanel[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.height){
+      content.style.height = null;
+    } else {
+      content.style.height = content.scrollHeight + "px";
+    }
+  });
+}
+
+var aboutPanel = document.getElementsByClassName("about-us-panel-button");
+
+for (i = 0; i < aboutPanel.length; i++) {
+  aboutPanel[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.height){
+      content.style.height = null;
+    } else {
+      content.style.height = content.scrollHeight + "px";
+    }
+  });
+}
